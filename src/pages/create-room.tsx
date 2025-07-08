165f275eb990a342d52fdc9eb3cd23ac915b2362
Oseas Moreto <oseas.moreto@uappi.com.br>
2025-07-08 15:01:32 -0300

    feat: add react router

---
+++ src/pages/create-room.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export function CreateRoom() {
+  return (
+    <div>
+      <div>Create Room</div>
+      <Link to="/room">Room</Link>
+    </div>
+  );
+}
